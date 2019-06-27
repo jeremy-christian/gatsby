81ae4ac66389338227e42fe1af0ba1577b04712f
Jeremy Christian <jeremy.christian.san@gmail.com>
2019-06-27 12:34:40 +0100

    Importing ScrabbleWordFinder

--- src/components/tic-tac-toe.js
+++ src/components/tic-tac-toe.js
@@ -1,44 +1,66 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import scrabbleHelpers from "../utils/scrabble_helpers.js"
 import "../styles/index.css"
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
 
 class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      tiles: Array(15).fill(Array(15).fill(null)),
+      squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
-  renderSquare(x, y) {
-    var modifier = scrabbleHelpers.modifiers[x][y]
+  handleClick(index) {
+    if (calculateWinner(this.state.squares)) return
+    const squares = this.state.squares.slice()
+    squares[index] = this.state.xIsNext ? "X" : "O"
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext })
+  }
+
+  // right here now we've extended React.Component we can start calling <Square />
+  // So this func returns a <Square /> with a element property 'value' set to i
+  renderSquare(i) {
     return (
-      <input
-        className={"square ".concat(modifier)}
-        type="text"
-        maxLength="1"
-        modifier={modifier}
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
       />
     )
   }
 
-  renderRow(y) {
-    return (
-      <div className="board-row">
-        {Array(15)
-          .fill("")
-          .map((_, i) => this.renderSquare(i, y))}
-      </div>
-    )
-  }
-
   render() {
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) status = "Winner: ".concat(winner)
+    else status = "Next player: ".concat(this.state.xIsNext ? "X" : "O")
+
     return (
       <div>
-        {Array(15)
-          .fill(null)
-          .map((_, i) => this.renderRow(i))}
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     )
   }
@@ -51,9 +73,37 @@ class Game extends React.Component {
         <div className="game-board">
           <Board />
         </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
       </div>
     )
   }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
 export default Game
+
+// ========================================
+
+// ReactDOM.render(<Game />, document.getElementById("root"))
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,21 @@ import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
-import Game from "../components/tic-tac-toe"
+import Game from "../components/scrabble"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 import "../styles/index.css"
 
 class BlogIndex extends React.Component {
   render() {
-    return <Game />
+    return (<Game /><div class="page-content">
+      <div class="wrapper">
+        <label for="letters">Letters</label>
+        <input type="text" id="letters"/>
+        <button onclick="findWords()">Find Words</button>
+        <pre id="words"></pre>
+      </div>
+    </div>)
   }
 }
 
