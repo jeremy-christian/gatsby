074cdb504c5baea1a1a81299c455fcdc942e8b6f
Jeremy Christian <jeremy.christian.san@gmail.com>
2019-06-27 12:07:01 +0100

    Board v1 Implemented

---
+++ src/components/scrabble.js
@@ -0,0 +1,109 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "../styles/index.css"
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
+
+  handleClick(index) {
+    if (calculateWinner(this.state.squares)) return
+    const squares = this.state.squares.slice()
+    squares[index] = this.state.xIsNext ? "X" : "O"
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext })
+  }
+
+  // right here now we've extended React.Component we can start calling <Square />
+  // So this func returns a <Square /> with a element property 'value' set to i
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) status = "Winner: ".concat(winner)
+    else status = "Next player: ".concat(this.state.xIsNext ? "X" : "O")
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+export default Game
+
+// ========================================
+
+// ReactDOM.render(<Game />, document.getElementById("root"))
--- src/components/tic-tac-toe.js
+++ src/components/tic-tac-toe.js
@@ -1,78 +1,44 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import scrabbleHelpers from "../utils/scrabble_helpers.js"
 import "../styles/index.css"
-// // react compoenents seem to return jsx at the end to render a page element
-// class Square extends React.Component {
-//   render() {
-//     // this means in the context of 'this', 'props' are your element's attributes?
-//     return (
-//       // I guess setState is a naitive react function?
-//       <button className="square" onClick={() => this.props.onClick()}>
-//         {this.props.value}
-//       </button>
-//     );
-//   }
-// }
-
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  )
-}
 
 class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
+      tiles: Array(15).fill(Array(15).fill(null)),
     }
   }
 
-  handleClick(index) {
-    if (calculateWinner(this.state.squares)) return
-    const squares = this.state.squares.slice()
-    squares[index] = this.state.xIsNext ? "X" : "O"
-    this.setState({ squares: squares, xIsNext: !this.state.xIsNext })
-  }
-
-  // right here now we've extended React.Component we can start calling <Square />
-  // So this func returns a <Square /> with a element property 'value' set to i
-  renderSquare(i) {
+  renderSquare(x, y) {
+    var modifier = scrabbleHelpers.modifiers[x][y]
     return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+      <input
+        className={"square ".concat(modifier)}
+        type="text"
+        maxLength="1"
+        modifier={modifier}
       />
     )
   }
 
-  render() {
-    const winner = calculateWinner(this.state.squares)
-    let status
-    if (winner) status = "Winner: ".concat(winner)
-    else status = "Next player: ".concat(this.state.xIsNext ? "X" : "O")
+  renderRow(y) {
+    return (
+      <div className="board-row">
+        {Array(15)
+          .fill("")
+          .map((_, i) => this.renderSquare(i, y))}
+      </div>
+    )
+  }
 
+  render() {
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {Array(15)
+          .fill(null)
+          .map((_, i) => this.renderRow(i))}
       </div>
     )
   }
@@ -85,37 +51,9 @@ class Game extends React.Component {
         <div className="game-board">
           <Board />
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
       </div>
     )
   }
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
-    }
-  }
-  return null
-}
-
 export default Game
-
-// ========================================
-
-// ReactDOM.render(<Game />, document.getElementById("root"))
